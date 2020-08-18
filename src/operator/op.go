a9ef42f8fb8ea7cdeedf1a3f9b651ace762ef407
hyeonseungho <seunghoking1@naver.com>
2020-08-18 13:31:50 +0900

    operator

---
+++ src/operator/op.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var a int
+	// a = 4
+	a := 4
+	b := 2
+
+	fmt.Printf("a&b = %v\n", a&b)
+	fmt.Printf("a|b = %v\n", a|b)
+	fmt.Printf("a^b = %v\n", a^b)
+}
